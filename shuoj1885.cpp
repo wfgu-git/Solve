deb5993ee7a2fe112e34f59ceb0283fef074f1c5
peter <peter.wfgu@gmail.com>
2017-02-21 01:17:39 +0800

    2017年 02月 21日 星期二 01:17:39 CST

--- shuoj1885.cpp
+++ shuoj1885.cpp
@@ -1,98 +1,90 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <cstring>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <stack>
-#include <iomanip>
-#include <cctype>
-#include <climits>
-#include <utility>
-#include <memory>
-#include <functional>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<map>
+#include<set>
+#include<cstring>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<stack>
+#include<iomanip>
+#include<cctype>
+#include<climits>
+#include<queue>
+#include<utility>
+#include<functional>
 using namespace std;
-typedef pair<int, int> PII;
-typedef vector<int> VI;
+typedef pair<int,int> PII;
 typedef long long ll;
 typedef unsigned long long ull;
 #define sc(a) scanf("%d",&a)
 #define sc2(a,b) scanf("%d%d",&a,&b)
 #define sc3(a,b,c) scanf("%d%d%d",&a,&b,&c)
-#define mp(a,b) make_pair(a,b)
+#define pr(a) printf("%d\n",a)
+#define MP(a,b) make_pair(a,b)
+#define PB(a) push_back(a)
 #define mset(x,i) memset(x,i,sizeof(x))
-#define pb(a) push_back(a)
 #define fi first
 #define se second
 #define lch l,m,rt<<1
 #define rch m+1,r,rt<<1|1
-const int mod = 1e9 + 7;
+const int mod = 1e9+7;
 const int INF = 0x3f3f3f3f;
 const double PI = acos(-1.0);
 const double eps = 1e-8;
 
-const int maxn = 1e5;
+const int maxn=1e5;
 int a[maxn];
 int bit[maxn];
-int N, m;
+int n,m;
 int lowbit(int x)
 {
-	return x & (-x);
+    return x & (-x);
 }
 
-void add(int idx, int v)
+void add(int idx, int k)
 {
-	while (idx <= N) {
-		bit[idx] += v;
-		idx += lowbit(idx);
-	}
+    for(int i=idx; i>0; i-=lowbit(i)){
+        bit[i] += k;
+    }
 }
 
-int GetSum(int idx)
+int query(int idx)
 {
-	int res = 0;
-	while (idx > 0) {
-		res += bit[idx];
-		idx -= lowbit(idx);
-	}
-	return res;
+    int res = 0;
+    while(idx <= n){
+        res += bit[idx];
+        idx += idx & -idx;
+    }
+    return res;
 }
 
-int GetValue(int idx) {
-	return GetSum(idx) - GetSum(idx - 1);
-}
 int main(void)
 {
-#ifdef LOCAL_TEST
-	freopen("data.in", "r", stdin);
-	freopen("data.out", "w", stdout);
-#endif
-	sc2(N, m);
-	for (int i = 1; i <= N; i++)  sc(a[i]);
-	//build
-	for (int i = 1; i <= N; i++) {
-		bit[i] = a[i];
-		for (int j = i - 1; j > i - lowbit(i); j--) {
-			bit[i] += a[j];
-		}
-	}
+    sc2(n,m);
+    for(int i=1;i<=n;i++){
+        sc(a[i]);
+    }
 
-	for (int i = 0; i < m; i++) {
-		int mark, l, r, k, v;
-		sc(mark);
-		if (mark == 1) {
-			sc3(l, r, v);
-			add(l, v);
-			add(r +lowbit(r), -v);
-		} else if (mark == 2) {
-			sc(k);
-			printf("%d\n", GetValue(k));
-		}
-	}
-	return 0;
+    while(m--){
+        int flag;
+        sc(flag);
+        if(flag == 1){
+            int l,r,k;
+            sc3(l,r,k);
+            add(r,k);
+            add(l-1,-k);
+            continue;
+        }
+        else if(flag == 2){
+            int index;
+            sc(index);
+            int ans=a[index]+query(index);
+            pr(ans);
+            continue;
+        }
+    }
+    return 0;
 }
