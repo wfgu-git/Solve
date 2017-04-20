ed3659c047aa9f97f2fdbb8b62e5655aaadcdb3d
Prgwf <peter.wfgu@gmail.com>
2017-04-20 11:55:28 +0800

    2017年04月20日 11:55:28

--- POJ2251.cpp
+++ POJ2251.cpp
@@ -21,13 +21,13 @@ typedef pair<int, int> PII;
 typedef long long ll;
 //typedef __int64 i64;
 typedef unsigned long long ull;
-typedef priority_queue<int, vector<int>, greater<int> > pq_int;
-typedef priority_queue<PII, vector<PII>, greater<PII> > pq_PII;
+typedef priority_queue<int, vector<int>, greater<int>> pq_int;
+typedef priority_queue<PII, vector<PII>, greater<PII>> pq_PII;
 #define iscanf(a) scanf("%d",&a)
 #define iscanf2(a,b) scanf("%d%d",&a,&b)
 #define iscanf3(a,b,c) scanf("%d%d%d",&a,&b,&c)
 #define mp(a,b) make_pair(a,b)
-#define mset(x,i) memset(x,i,sizeof(x))
+#define clr(x,i) memset(x,i,sizeof(x))
 #define pb(a) push_back(a)
 #define fi first
 #define se second
@@ -41,39 +41,112 @@ const int INF = 0x3f3f3f3f;
 const double pi = acos(-1.0);
 const double eps = 1e-8;
 
-
+const int maxn = 35;
+struct Node {
+    int r, c, layer;
+    Node() {}
+    Node(int _l, int _r, int _c): r(_r), c(_c), layer(_l) {}
+};
+bool flag;
+int ret;
 int L, R, C;
-char pic[35][35][35];
+char grid[maxn][maxn][maxn];
+int dist[maxn][maxn][maxn];
 //[level][row][column]
-
-void dfs(int layer, int r, int c, int step) {
-
+int dr[] = {-1, 1, 0, 0};
+int dc[] = {0, 0, -1, 1};
+int dl[] = {-1, 1};
+// dl = -1  up
+// dl =  1  down
+bool canMove(int l, int r, int c) {
+    if (l < 0 || l >= L || r < 0 || r >= R || c < 0 || c >= C)  return false;
+    if (grid[l][r][c] == '#')  return false;
+    return true;
 }
 
+Node findStart() {
+    Node s;
+    for (int l = 0; l < L; l++) {
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (grid[l][r][c] == 'S') {
+                    s.r = r, s.c = c, s.layer = l;
+                    return s;
+                }
+            }
+        }
+    }
+}
+
+void Bfs() {
+    clr(dist, -1);
+    Node start = findStart();
+    dist[start.layer][start.r][start.c] = 0;
+    queue<Node> q;
+    q.push(start);
+
+    while (!q.empty()) {
+        Node cur = q.front();
+        q.pop();
+        int r = cur.r;
+        int c = cur.c;
+        int l = cur.layer;
+
+        // up or down
+        for (int i = 0; i < 2; i++) {
+            int nl = l + dl[i];
+            int nr = r;
+            int nc = c;
+
+            if (canMove(nl, nr, nc) == true && dist[nl][nr][nc] == -1) {
+                dist[nl][nr][nc] = dist[l][r][c] + 1;
+                if (grid[nl][nr][nc] == 'E') {
+                    flag = true;
+                    ret = dist[nl][nr][nc];
+                    return ;
+                }
+                q.push(Node(nl, nr, nc));
+            }
+        }
+        // the same layer
+        for (int i = 0; i < 4; i++) {
+            int nl = l;
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            // printf("r = %d  c = %d  l = %d\n", nr, nc, nl);
+            if (canMove(nl, nr, nc) == true && dist[nl][nr][nc] == -1) {
+                dist[nl][nr][nc] = dist[l][r][c] + 1;
+                // printf("row = %d  col = %d layer = %d  step = %d\n", nl, nr, nc, dist[nl][nr][nc]);
+                if (grid[nl][nr][nc] == 'E') {
+                    // printf("%d %d %d\n", nl, nr, nc);
+                    flag = true;
+                    ret = dist[nl][nr][nc];
+                    return;
+                }
+                q.push(Node(nl, nr, nc));
+            }
+        }
+    }// for
+
+}// while
+
+
 int main(int argc, char const *argv[]) {
-	// freopen("data.in","r",stdin);
-	// freopen("data.out","w",stdout);
-	while(iscanf3(L, R, C) == 3 && (L + R + C)) {
-		for(int l = 0; l < L; l++) {
-			for(int r = 0; r < R; r++) {
-				scanf("%s", pic[l][r]);
-			}
-		}
+    // freopen("data.in","r",stdin);
+    // freopen("data.out","w",stdout);
+    while (iscanf3(L, R, C) == 3 && (L + R + C)) {
+        clr(grid, -1);
+        for (int l = 0; l < L; l++) {
+            for (int r = 0; r < R; r++) {
+                scanf("%s", grid[l][r]);
+            }
+        }
 
-		int step = 0;
-		for(int i = 0; i < L; i++) {
-			for(int j = 0; j < R; j++) {
-				for(int k = 0; k < C; k++) {
-					if(pic[i][j][k] == 'S') {
-						dfs(i, j, k, step);
-					}
-				}
-			}
-		}
-
-		if(!flag)  printf("Trapped!\n");
-		else  printf("Escaped in %d minute(s).\n", step);
-	}
-	return 0;
+        flag = false;
+        Bfs();
+        if (!flag)  printf("Trapped!\n");
+        else  printf("Escaped in %d minute(s).\n", ret);
+    }
+    return 0;
 }
 
