1be27844b565a70494c68d69eb005e988503995c
Prgwf <peter.wfgu@gmail.com>
2017-04-25 15:06:06 +0800

    2017年04月25日 15:06:06

--- HDU1495.cpp
+++ HDU1495.cpp
@@ -50,18 +50,85 @@ const double eps = 1e-8;
 
 int s, m, n;
 int cnt;
-void dfs() {
-    if (s == 0)
-    }
-int main(int argc, char const *argv[]) {
-    while (~iscanf3(s, m, n) && (s + m + n)) {
-        if (s & 1 == 1) {
-            printf("No\n");
-            continue;
-        } else {
-            cnt = 0;
+const int maxn = 105;
+int dist[maxn][maxn][maxn];
+struct cola {
+    int v, curv;
+    cola() {}
+    cola(int _v, int _curv): v(_v), curv(_curv) {}
+};
+struct Node {
+    cola v0, v1, v2;
+    Node() {}
+    Node(cola s, cola m, cola n): v0(s), v1(m), v2(n) {}
+};
 
+void pour(int &x, const int &v, int &cur) {
+    if(!x)  return ;
+    int left = v - cur;
+    if(x >= left) {
+        cur += left;
+        x -= left;
+    } else {
+        cur += x;
+        x = 0;
+    }
+}
+
+int ret;
+void bfs() {
+    clr(dist, -1);
+    //当前杯子中的水
+    dist[s][0][0] = 0;
+    queue<Node> q;
+    q.push(Node(cola(s, s), cola(m, 0), cola(n, 0)));
+    if(s & 1)  return;
+    int target = s / 2;
+    while(!q.empty()) {
+        Node cur = q.front(); q.pop();
+        //对于每一桶 都有两个选择  全部倒给另两个中的一个
+        cola v[3];
+        for(int i = 0; i < 3; i++) {
+            v[0] = cur.v0, v[1] = cur.v1, v[2] = cur.v2;
+            pour(v[i].curv, v[(i + 1) % 3].v, v[(i + 1) % 3].curv);
+            if(dist[v[0].curv][v[1].curv][v[2].curv] == -1) {
+                dist[v[0].curv][v[1].curv][v[2].curv] = dist[cur.v0.curv][cur.v1.curv][cur.v2.curv] + 1;
+
+                if(v[0].curv == target)  {
+                    ret = dist[v[0].curv][v[1].curv][v[2].curv];
+                    return;
+                }
+                q.push(Node(cola(v[0].v, v[0].curv), cola(v[1].v, v[1].curv), cola(v[2].v, v[2].curv)));
+            }
+            //
+            v[0] = cur.v0, v[1] = cur.v1, v[2] = cur.v2;
+            pour(v[i].curv, v[(i + 2) % 3].v, v[(i + 2) % 3].curv);
+            if(dist[v[0].curv][v[1].curv][v[2].curv] == -1) {
+                dist[v[0].curv][v[1].curv][v[2].curv] = dist[cur.v0.curv][cur.v1.curv][cur.v2.curv] + 1;
+
+                if(v[0].curv == target)  {
+                    ret = dist[v[0].curv][v[1].curv][v[2].curv];
+                    return;
+                }
+
+                q.push(Node(cola(v[0].v, v[0].curv), cola(v[1].v, v[1].curv), cola(v[2].v, v[2].curv)));
+            }
         }
+    }
+}
+
+int gcd(int a, int b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+int main(int argc, char const *argv[]) {
+    while(~iscanf3(s, m, n) && (s + m + n)) {
+        ret = INF;
+        bfs();
+        if(ret == INF)  printf("NO\n");
+        else printf("%d\n", ret);
+        // pour(s, m, n);
+        // printf("%d %d %d\n", s, m, n);
     }
     return 0;
 }
