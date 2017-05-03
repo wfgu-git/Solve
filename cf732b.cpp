ee1f9b41b1bbc4fd76b78e4c5c5e73f8f86026d4
Prgwf <peter.wfgu@gmail.com>
2017-05-03 23:08:59 +0800

    2017年 05月 03日 星期三 23:08:59 CST

---
+++ cf732b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> PII;
+const int INF = 0x3f3f3f3f;
+
+int a[550];
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    int n, k;
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    a[0] = k;
+    int cnt = 0;
+    for(int i = 1; i <= n; i++) {
+        if(a[i] + a[i - 1] >= k) continue;
+        else {
+            int walkNeed = k - a[i] - a[i - 1];
+            cnt += walkNeed;
+            a[i] += walkNeed;
+        }
+    }
+    cout << cnt << endl;
+    for(int i = 1; i <= n; i++) {
+        if(i != 1) cout << " ";
+        cout << a[i];
+    }
+    cout << endl;
+    return 0;
+}
