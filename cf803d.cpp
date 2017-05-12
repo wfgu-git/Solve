95e19214eae4eb56837c78b1a21cae853f3303b0
Prgwf <peter.wfgu@gmail.com>
2017-05-12 15:11:38 +0000

    Fri May 12 15:11:38 UTC 2017

--- cf803d.cpp
+++ cf803d.cpp
@@ -5,10 +5,9 @@ const int inf = 0x3f3f3f3f;
 string s;
 vector<int> End;
 int check(int len) {
-    int n = End.size();
     int cnt = 0;
     int temp = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < (int)End.size(); i++) {
         if (len < End[i]) return inf;
         if (temp + End[i] <= len) temp += End[i];
         else ++cnt, temp = End[i];
@@ -21,27 +20,26 @@ int main(int argc, char const *argv[]) {
     cin >> k;
     cin.get();
     getline(cin, s);
+    s.push_back(' ');
     int strsz = s.size();
     for (int i = 0; i < strsz; i++) {
         if (s[i] == ' ' || s[i] == '-')
             End.push_back(i);
     }
     for (int i = End.size() - 1; i >= 1; i--) End[i] -= End[i - 1];
-    End[0] += 1;
+    End[0]++, End[End.size() - 1]--;
+    // for(auto x : End) cout << x << endl;
 
     int l = 0, r = strsz;
     while (l < r) {
         int mid = l + (r - l) / 2;
-        cout << l << "  " << r << "   ";
-        cout << "len = " << check(mid) << endl;
-        if (check(mid) >= k)
-            l = mid + 1;
-        else
+        // cout << l << "  " << r << "   ";
+        // cout << "len = " << check(mid) << endl;
+        if (check(mid) <= k)
             r = mid;
+        else
+            l = mid + 1;
     }
     cout << r << endl;
     return 0;
 }
-
-
-
