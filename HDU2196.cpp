c6c267cdd5e17ba214352a9d1b3974b7c052e941
Prgwf <peter.wfgu@gmail.com>
2017-05-17 23:44:10 +0000

    Wed May 17 23:44:10 UTC 2017

--- HDU2196.cpp
+++ HDU2196.cpp
@@ -1,90 +1,100 @@
-/*
-Life has never been fucking so easy.
-Coding...
-...WeiFeng
-*/
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <cstring>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <stack>
-#include <iomanip>
-#include <cctype>
-#include <climits>
-#include <utility>
-#include <memory>
-#include <functional>
+#include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> PII;
-typedef long long ll;
-//typedef __int64 i64;
-typedef unsigned long long ull;
-#define pq priority_queue
-#define iscanf(a) scanf("%d", &a)
-#define iscanf2(a,b) scanf("%d%d",&a, &b)
-#define iscanf3(a,b,c) scanf("%d%d%d",&a, &b, &c)
-#define mo(a,b) (((a)%(b)+(b))%(b))
-#define all(x) (x).begin(), (x).end()
-#define UNIQUE(v) sort(all(x)); v.erase(unique(all(v)), end())
-#define mp(a,b) make_pair(a,b)
-#define clr(x,i) memset(x,i,sizeof(x))
-#define pb(a) push_back(a)
-#define fi first
-#define se second
-#define lch l,m,rt<<1
-#define rch m+1,r,rt<<1|1
-#define rep(i,x,n) for(int i=x;i<n;i++)
-#define Max(a,b)  ((a)>(b)?(a):(b))
-#define Min(a,b)  ((a)<(b)?(a):(b))
-const int MOD = 1000000007;
-const int INF = 0x3f3f3f3f;
-const double pi = acos(-1.0);
-const double eps = 1e-8;
 
-//gcd lcm
-int gcd(int a, int b) {
-	return b ? gcd(b, a % b) : a;
-}
-
-int lcm(int a, int b) {
-	return a / gcd(a, b) * b;
-}
-
-int n;
-int Head[maxn];
+const int maxn = 1e5 + 10;
 struct Edge {
-	int u, v, d;
-	int next = -1;
-} e[maxn];
+    int u, v, d, next;
+} e[maxn << 1];
+int head[maxn], tot, n, ret[maxn];
+int maxdist[maxn], maxid[maxn];
+int secdist[maxn], secid[maxn];
 void add_edge(int u, int v, int d) {
-	e[tot].u = u, e[tot].v = v, e[tot].d = d;
-	e[tot].next = Head[u];
-	Head[u] = tot++;
-	e[tot].u = v, e[tot].v = u, e[tot].d = d;
-	e[tot].next = Head[v];
-	Head[v] = tot++;
+    e[tot].u = u;
+    e[tot].v = v;
+    e[tot].d = d;
+    e[tot].next = head[u];
+    head[u] = tot++;
 }
 
+void init() {
+    tot = 0;
+    memset(head, -1, sizeof(head));
+}
 
-void dfs()
+void dfs_subtree(int u, int p) {
+    maxdist[u] = 0;
+    secdist[u] = 0;
+    for (int i = head[u]; i != -1; i = e[i].next) {
+        int v = e[i].v;
+        int d = e[i].d;
+        if (v == p) continue;
+        dfs_subtree(v, u);
+        if (secdist[u] < maxdist[v] + d) {
+            secdist[u] = maxdist[v] + d;
+            secid[u] = v;
+            if (secdist[u] > maxdist[u]) {
+                swap(secdist[u], maxdist[u]);
+                swap(secid[u], maxid[u]);
+            }
+        }
+    }
+}
+
+void dfs_fa(int u, int p) {
+    for (int i = head[u]; i != -1; i = e[i].next) {
+        int v = e[i].v;
+        int d = e[i].d;
+        if (v == p) continue;
+        if (v == maxid[u]) {
+            if (secdist[v] < secdist[u] + d) {
+                secdist[v] = secdist[u] + d;
+                secid[v] = u;
+                if (secdist[v] > maxdist[v]) {
+                    swap(secdist[v], maxdist[v]);
+                    swap(secid[v], maxid[v]);
+                }
+            }
+        } else {
+            if (secdist[v] < maxdist[u] + d) {
+                secdist[v] = maxdist[u] + d;
+                secid[v] = u;
+                if (secdist[v] > maxdist[v]) {
+                    swap(secdist[v], maxdist[v]);
+                    swap(secid[v], maxid[v]);
+                }
+            }
+        }
+        dfs_fa(v, u);
+    }
+}
+
+void debug() {
+    for (int i = 1; i <= n; i++) {
+        cout << "v = " << i << "  " << "max = " << maxdist[i] << "  sec = " << secdist[i] << endl;
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << "maxid = " << maxid[i] << "  secid = " << secid[i] << endl;
+    }
+}
+
 int main(int argc, char const *argv[]) {
-	// freopen("data.in","r",stdin);
-	// freopen("data.out","w",stdout);
-	while(~iscanf("%d", &n)) {
-		clr(e, 0); clr(Head, -1);
-		int p = 0;
-		for(int i = 2; i <= n; i++) {
-			int v, d;
+    while (scanf("%d", &n) == 1) {
+        init();
+        for (int u = 2; u <= n; u++) {
+            int v, d;
+            scanf("%d%d", &v, &d);
+            add_edge(u, v, d);
+            add_edge(v, u, d);
+        }
+        dfs_subtree(1, 0);
+        // debug();
+        dfs_fa(1, 0);
+        // cout << "\n\n";
+        // debug();
 
-		}
-	}
-	return 0;
+        for (int i = 1; i <= n; i++) {
+            printf("%d\n", maxdist[i]);
+        }
+    }
+    return 0;
 }
-
--- POJ2342.cpp
+++ POJ2342.cpp
@@ -1,126 +1,63 @@
-/*
-Life has never been fucking so easy.
-Coding...
-...WeiFeng
-*/
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <cstring>
 #include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <stack>
-#include <iomanip>
-#include <cctype>
-#include <climits>
-#include <utility>
-#include <memory>
-#include <functional>
+#include <cstring>
+#include <algorithm>
 using namespace std;
-typedef pair<int, int> PII;
-typedef long long ll;
-//typedef __int64 i64;
-typedef unsigned long long ull;
-#define pq priority_queue
-#define iscanf(a) scanf("%d", &a)
-#define iscanf2(a,b) scanf("%d%d",&a, &b)
-#define iscanf3(a,b,c) scanf("%d%d%d",&a, &b, &c)
-#define mo(a,b) (((a)%(b)+(b))%(b))
-#define all(x) (x).begin(), (x).end()
-#define UNIQUE(v) sort(all(x)); v.erase(unique(all(v)), end())
-#define mp(a,b) make_pair(a,b)
-#define clr(x,i) memset(x,i,sizeof(x))
-#define pb(a) push_back(a)
-#define fi first
-#define se second
-#define lch l,m,rt<<1
-#define rch m+1,r,rt<<1|1
-#define rep(i,x,n) for(int i=x;i<n;i++)
-#define Max(a,b)  ((a)>(b)?(a):(b))
-#define Min(a,b)  ((a)<(b)?(a):(b))
-const int MOD = 1000000007;
-const int INF = 0x3f3f3f3f;
-const double pi = acos(-1.0);
-const double eps = 1e-8;
 
-//gcd lcm
-int gcd(int a, int b) {
-	return b ? gcd(b, a % b) : a;
-}
-
-int lcm(int a, int b) {
-	return a / gcd(a, b) * b;
-}
-
-const int maxn = 10000;
-int N, tot;
-int w[maxn], Head[maxn];
-int dp[maxn][2];
-// 0 -- > not come
-// 1 -- > come
-// dp[i][0] = max{dp[j][0], dp[j][1]}
-// dp[i][1] = max{dp[j][0]}
+const int maxn = 6005;
 struct Edge {
-	int from, to;
+	int u, v;
 	int next;
-} edge[maxn];
+} edge[maxn << 1];
+int n, tot, head[maxn];
+int val[maxn];
+int dp[maxn][2];
 void add_edge(int u, int v) {
-	edge[tot].from = u, edge[tot].to = v;
-	edge[tot].next = Head[u];
-	Head[u] = tot++;
-	edge[tot].from = v, edge[tot].to = u;
-	edge[tot].next = Head[v];
-	Head[v] = tot++;
+	edge[tot].u = u;
+	edge[tot].v = v;
+	edge[tot].next = head[u];
+	head[u] = tot++;
 }
 
+int solve(int u, int p, bool take) {
+	if (dp[u][take] != -1)
+		return dp[u][take];
 
-int Dfs(int u, int pre, bool is_come) {
-	if(dp[u][is_come] != -1) {
-		return dp[u][is_come];
-	}
-	dp[u][is_come] = 0;
-	if(is_come) {
-		dp[u][is_come] += w[u];
-		for(int i = Head[u]; i != -1; i = edge[i].next) {
-			int v = edge[i].to;
-			if(v != pre) {
-				dp[u][is_come] += Dfs(v, u, 0);
-			}
+	dp[u][take] = 0;
+	if (take) {
+		dp[u][take] = val[u];
+		for (int i = head[u]; i != -1; i = edge[i].next) {
+			int v = edge[i].v;
+			if (v == p) continue;
+			dp[u][1] += solve(v, u, 0);
 		}
 	} else {
-		for(int i = Head[u]; i != -1; i = edge[i].next) {
-			int v = edge[i].to;
-			if(v != pre) {
-				dp[u][is_come] += Max(Dfs(v, u, 0), Dfs(v, u, 1));
-			}
+		dp[u][take] = 0;
+		for (int i = head[u]; i != -1; i = edge[i].next) {
+			int v = edge[i].v;
+			if (v == p) continue;
+			dp[u][0] += max(solve(v, u, 0) , solve(v, u, 1));
 		}
 	}
-	return dp[u][is_come];
+	return dp[u][take];
+}
+void init() {
+	tot = 0;
+	memset(head, -1, sizeof(head));
+	memset(dp, -1, sizeof(dp));
 }
 
 int main(int argc, char const *argv[]) {
-	// freopen("data.in","r",stdin);
-	// freopen("data.out","w",stdout);
-	while(~iscanf(N)) {
-		clr(w, 0), clr(edge, 0), clr(Head, -1), clr(dp, -1);
-		for(int i = 1; i <= N; i++) {
-			iscanf(w[i]);
-		}
-
-		tot = 0;
+	while (scanf("%d", &n) != EOF) {
+		init();
+		for (int i = 1; i <= n; i++) {scanf("%d", &val[i]);}
 		int u, v;
-		while(iscanf2(u, v) && (u + v)) {
+		while (scanf("%d%d", &u, &v)) {
+			if (u == 0 && v == 0) break;
 			add_edge(u, v);
+			add_edge(v, u);
 		}
-
-		int ret = Max(Dfs(1, 1, 0), Dfs(1, 0, 0));
-		printf("%d\n", ret);
+		printf("%d\n", max(solve(1, 0, 0), solve(1, 0, 1)));
 	}
 	return 0;
 }
-
