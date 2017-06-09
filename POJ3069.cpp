ea06cbfd178a4124f3ee3fbe59356a4b64b0d3f5
Prgwf <peter.wfgu@gmail.com>
2017-06-09 02:15:11 +0000

    Fri Jun  9 02:15:10 UTC 2017

--- POJ3069.cpp
+++ POJ3069.cpp
@@ -1,48 +1,28 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<vector>
-#include<map>
-#include<set>
-#include<cstring>
-#include<cstdio>
-#include<cmath>
-#include<cstdlib>
-#include<stack>
-#include<iomanip>
-#include<cctype>
-#include<climits>
-#include<queue>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-vector<int> a;
 
+int a[1002];
 int main()
 {
-    ios::sync_with_stdio(false);
-    int r,n;
-    while(cin>>r>>n&&n!=-1&&r!=-1){
-        a.clear();
-        int x;
-        for(int i=0;i<n;i++){
-            cin>>x;
-            a.push_back(x);
-        }
-        sort(a.begin(),a.end());
-        int ans=0,i=0;
-        while(i<n){
-            int tmp=a[i];
+	ios::sync_with_stdio(false);
 
-            while(i<n&&a[i]<=tmp+r) i++; //go ahead
-
-            int p=a[i-1];
-
-            while(i<n&&a[i]<=p+r) i++;
-
-            ans++;
-        }
-        cout<<ans<<endl;
-    }
-    return 0;
+	int R, n;
+	while (cin >> R >> n){
+		if (R == -1 && n == -1) break;
+		for (int i = 0; i < n; i++) { cin >> a[i]; }
+		sort(a, a + n);
+		
+		int i = 0, ans = 0;
+		while (i < n){
+			int uncover = a[i++];
+			while (i < n && a[i] <= uncover + R) i++;
+			int mark = a[i - 1];
+			while (i < n && a[i] <= mark + R) i++;
+			ans++;
+		}
+		cout << ans << endl;
+	}
+	return 0;
 }
