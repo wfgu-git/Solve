8a2b64a10f2ceeca925759dd3955d616b3703554
Prgwf <peter.wfgu@gmail.com>
2017-06-18 02:31:20 +0000

    Sun Jun 18 02:31:20 UTC 2017

--- Codeforces/419/C.cpp
+++ Codeforces/419/C.cpp
@@ -4,19 +4,17 @@ using namespace std;
 const int inf = 0x3f3f3f3f;
 int n, m;
 int g[505][505];
-bool row_del(int row)
-{
+bool row_del(int row) {
 	bool flag = true;
 	for (int i = 0; i < m; i++) {
-		if (g[row][i] <= 0) { 
+		if (g[row][i] <= 0) {
 			flag = false;
 			break;
 		}
 	}
 	return flag;
 }
-bool col_del(int col)
-{
+bool col_del(int col) {
 	bool flag = true;
 	for (int i = 0; i < n; i++) {
 		if (g[i][col] <= 0) {
@@ -27,23 +25,56 @@ bool col_del(int col)
 	return flag;
 }
 vector < int > R, C;
-int main()
-{
+int main() {
 	ios::sync_with_stdio(false);
 
 	while (cin >> n >> m) {
-		for (int i = 0; i < n; i++) 
+		for (int i = 0; i < n; i++)
 			for (int j = 0; j < m; j++)
 				cin >> g[i][j];
-		
+
+		if (m < n) {
+			for (int c = 0; c < m; c++) {
+				while (col_del(c)) {
+					for (int i = 0; i < n; i++) {
+						g[i][c]--;
+					}
+					C.push_back(c + 1);
+				}
+			}
+			for (int r = 0; r < n; r++) {
+				while (row_del(r)) {
+					for (int i = 0; i < m; i++) {
+						g[r][i]--;
+					}
+					R.push_back(r + 1);
+				}
+			}
+
+			bool flag = true;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (g[i][j] != 0)
+						flag = false;
+				}
+			}
+			if (!flag) cout << -1 << endl;
+			else {
+				int ans = R.size() + C.size();
+				cout << ans << endl;
+				for (auto x : C) cout << "col " << x << endl;
+				for (auto x : R) cout << "row " << x << endl;
+			}
+			continue;
+		}
 		for (int r = 0; r < n; r++) {
 			while (row_del(r)) {
-				for (int i = 0; i < m; i++){
+				for (int i = 0; i < m; i++) {
 					g[r][i]--;
 				}
 				R.push_back(r + 1);
 			}
-		}	
+		}
 
 		for (int c = 0; c < m; c++) {
 			while (col_del(c)) {
--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 110002;
-char Ma[maxn << 1];
-int Mp[maxn << 1];
-void Manacher(const string &s)
-{
-    int l = 0;
-    Ma[l++] = '$';
-    Ma[l++] = '#';
-    for (auto ch : s) {
-        Ma[l++] = ch;
-        Ma[l++] = '#';
-    }
-    Ma[l] = 0;
-    int mx = 0, id = 0;
-    for (int i = 0; i < l; i++) {
-        if (mx > i) {
-            Mp[i] = min(Mp[2 * id - i], mx - i);
-        } else {
-            Mp[i] = 1;
-        }
-        while (Ma[i + Mp[i]] == Ma[i - Mp[i]] ) ++Mp[i];
-        if (i + Mp[i] > mx) {
-            mx = i + Mp[i];
-            id = i;
-        }
-    }
-}
+
+map < int, int > cnt;
 int main()
 {
-    ios::sync_with_stdio(false);
-
-    string s;
-    while (cin >> s) {
-        Manacher(s);
-        int ans = 0;
-        for (int i = 0; i < 2 * s.size() + 2; i++) {
-            ans = max(ans, Mp[i] - 1);
+    int n;
+    while (cin >> n) {
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            cnt[x]++;
+        }
+        int ans, Max = -1;
+        for (auto kv : cnt) {
+                if (kv.second > Max) {
+                    Max = kv.second;
+                    ans = kv.first;
+                }
         }
         cout << ans << endl;
     }
