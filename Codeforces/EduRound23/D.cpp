3e58aa8eb62d40e87b7c41c3adf253a7e46c4c0a
Prgwf <peter.wfgu@gmail.com>
2017-06-19 13:33:45 +0000

    Mon Jun 19 13:33:45 UTC 2017

--- Codeforces/EduRound23/D.cpp
+++ Codeforces/EduRound23/D.cpp
@@ -6,6 +6,7 @@ int a[maxn];
 int l[maxn], r[maxn];
 int main()
 {
+    ios::sync_with_stdio(false);
     int n;
     cin >> n;
     for_each(a, a + n, [](int &x) { cin >> x; });
@@ -13,34 +14,39 @@ int main()
     for (int i = 0; i < n; i++) r[i]  = l[i] = i;
     for (int i = 1; i < n; i++) { 
         int now = i;
-        while (now >= 1 && a[i] >= a[now - 1]) { now = l[now - 1]; }
+        while (now >= 1 && a[i] > a[now - 1]) { now = l[now - 1]; }
         l[i] = now;
     }
     for (int i = n - 2; i >= 0; i--) {
         int now = i;
-        while (now < n - 1 && a[i] > a[now + 1]) { now = r[now + 1]; }
+        while (now < n - 1 && a[i] >= a[now + 1]) { now = r[now + 1]; }
         r[i] = now;
     }
+    // for (int i = 0; i < n; i++) {
+    //     cout << a[i] << "  -- > (" << l[i] << ", " << r[i] << ")\n";
+    // }
     ll sigma_max = 0;
     for (int i = 0; i < n; i++) {
-        sigma_max += (i - l[i] + 1) * (r[i] - i + 1) * a[i];
+        sigma_max += (ll)(i - l[i] + 1) * (ll)(r[i] - i + 1) * (ll)a[i];
     }
 
     for (int i = 0; i < n; i++) r[i]  = l[i] = i;
     for (int i = 1; i < n; i++) {
         int now = i;
-        while (now >= 1 && a[i] <= a[now - 1]) { now = l[now - 1]; }
+        while (now >= 1 && a[i] < a[now - 1]) { now = l[now - 1]; }
         l[i] = now;
     }
     for (int i = n - 2; i >= 0; i--) {
         int now = i;
-        while (now < n - 1 && a[i] < a[now + 1]) { now = r[now + 1]; }
+        while (now < n - 1 && a[i] <= a[now + 1]) { now = r[now + 1]; }
         r[i] = now;
     }
-    ll sigma_min = 0;
+    // for (int i = 0; i < n; i++) {
+    //     cout << a[i] << "  -- > (" << l[i] << ", " << r[i] << ")\n";
+    // }
     for (int i = 0; i < n; i++) {
-        sigma_min += (i - l[i] + 1) * (r[i] - i + 1) * a[i];
+        sigma_max -= (ll)(i - l[i] + 1) * (ll)(r[i] - i + 1) * (ll)a[i];
     }
-    cout << sigma_max - sigma_min << endl;
+    cout << sigma_max << endl;
     return 0;
 }
---
+++ POJ/POJ2559.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+const int N = 1e5;
+int a[N + 5], l[N + 5], r[N + 5];
+ll ans;
+void solve(const int &n) {
+    for (int i = 0; i < n; i++) l[i] = r[i] = i;
+
+    for (int i = 1; i < n; i++) {
+        int now = i;
+        while (now >= 1 && a[i] <= a[now - 1]) now = l[now - 1];
+        l[i] = now;
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        int now = i;
+        while (now < n - 1 && a[i] <= a[now + 1]) now = r[now + 1];
+        r[i] = now;
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    int n;
+    while (cin >> n && n) {
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        solve(n);
+        ans = -1;
+
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " --> (" << l[i] << ", " << r[i] << ")" << endl;
+            ans = max(ans, (ll)a[i] * ((ll)r[i] - (ll)l[i] + (ll)1));
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
