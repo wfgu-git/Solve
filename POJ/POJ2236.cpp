5b62fd559690594da9dafbb42c094156ce7d50e1
Prgwf <peter.wfgu@gmail.com>
2017-06-30 22:11:13 +0000

    Fri Jun 30 22:11:13 UTC 2017

--- POJ/POJ2236.cpp
+++ POJ/POJ2236.cpp
@@ -1,62 +1,48 @@
 #include <iostream>
-#include <algorithm>
-#include <cstdio>
 #include <cstring>
+#include <algorithm>
 using namespace std;
-const int maxn = 1111;
-struct Point {
-    int x, y;
-    int dis(const Point &rhs) const  {
-        int dx = x - rhs.x;
-        int dy = y - rhs.y;
-        return dx * dx + dy * dy;
-    }
-    Point() {}
-    Point(int a, int b) : x(a), y(b) {}
-} point[maxn + 5];
-int n, d;
+const int maxn = 1000;
+struct Point {int x, y;} point[maxn + 5];
 int fa[maxn + 5];
-void init(const int &n) {
+int getd(const Point &a, const Point &b) {
+    int dx = a.x - b.x;
+    int dy = a.y - b.y;
+    return dx * dx + dy * dy;
+}
+void init(int n) {
     for (int i = 1; i <= n; i++) fa[i] = i;
 }
 int fnd(int x) {
     if (x != fa[x]) fa[x] = fnd(fa[x]);
     return fa[x];
 }
-void merge(int a, int b) {
-    int dis = point[a].dis(point[b]);
+void merge(int a, int b, int maxd) {
+    int d = getd(point[a], point[b]);
     a = fnd(a);
     b = fnd(b);
-    if (dis <= d * d && a != b) {
-        fa[a] = b;
-    }
+    if (d <= maxd * maxd && a != b) fa[a] = b;
 }
 bool vis[maxn + 5];
 int main() {
-    cin >> n >> d;
+    int n, maxd;
+    cin >> n >> maxd;
+    for (int i = 1; i <= n; i++) cin >> point[i].x >> point[i].y; 
     init(n);
-    for (int i = 1; i <= n; i++) {
-        cin >> point[i].x >> point[i].y;
-    }
-    memset(vis, 0, sizeof(vis));
     char op;
-    int a, b;
     while (cin >> op) {
-        if (op == '0') {
-            cin >> a;
-            vis[a] = true;
+        if (op == 'O') {
+            int x;
+            cin >> x;
+            vis[x] = true;
             for (int i = 1; i <= n; i++) {
-                if (i == a) continue;
-                if (!vis[i]) continue;
-                merge(a, i);
+                if (i != x && vis[i]) merge(i, x, maxd);
             }
         } else {
+            int a, b;
             cin >> a >> b;
-            if (fnd(a) == fnd(b)) {
-                cout << "SUCCESS" << endl;
-            } else {
-                cout << "FAIL" << endl;
-            }
+            if (fnd(a) == fnd(b)) cout << "SUCCESS" << endl;
+            else cout << "FAIL" << endl;
         }
     }
     return 0;
