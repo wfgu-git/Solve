a23b90bf57939ad1d9ed6b613ff383ab03b26411
Prgwf <peter.wfgu@gmail.com>
2017-07-11 22:24:17 +0800

    add...

--- SummerShu2017/K.cpp
+++ SummerShu2017/K.cpp
@@ -1,30 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int inf = INT_MAX;
-const int maxn = 1e5;
-int n, c;
-int a[maxn + 1];
-bool check(int d) {
-  int used = 0;
-  for (int i = 1; i < n; ++i) {
-    int cur = used + 1;
-    while (cur < n && a[cur] - a[used] < d) ++cur;
-    if (cur == n) return false;
-    used = cur;
+const int inf = 10000;
+const int maxn = 100000;
+struct Goods {
+  int w, c;
+
+  bool operator < (const Goods &rhs) const {
+    if (c == rhs.c) {
+      return w > rhs.w;
+    }
+    return c < rhs.c;
   }
-  return true;
+} info[maxn + 1];
+int n, m, nn, mm;
+bool check(int x) {
+  int cnt = 0;
+  for (int i = 0; i < n; ++i) {
+    if (mm - info[i].c >= 0 && info[i].w >= x) {
+      mm -= info[i].c;
+      ++cnt;
+    }
+    if (mm <= 0) break;
+  }
+  return cnt >= nn;
 }
 int main() {
-  while (scanf("%d%d", &n, &c) == 2) {
-    for_each(a, a + n, [] (int &x) {scanf("%d", &x);});
-    sort(a, a + n);
-    int lb = 0, ub = inf;
+  int T;
+  scanf("%d", &T);
+  for (int cas = 1; cas <= T; ++cas) {
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; ++i) {
+      scanf("%d%d", &info[i].w, &info[i].c);
+    }
+    sort(info, info + n);
+    nn = 0;
+    mm = m;
+    for (int i = 0; i < n; ++i) {
+      if (mm - info[i].c >= 0) {
+        ++nn;
+        mm -= info[i].c;
+      }
+    }
+
+    int lb = 0, ub = inf + 2;
     while (ub - lb > 1) {
+      mm = m;
       int mid = lb + (ub - lb) / 2;
       if (check(mid)) lb = mid;
       else ub = mid;
     }
-    printf("%d\n", lb);
+    printf("%d %d\n", nn, lb);
   }
   return 0;
 }
