6d79a420c12ce6f82434784512f5c5f47a90bb4a
Prgwf <peter.wfgu@gmail.com>
2017-07-28 17:12:17 +0800

    add...

--- NYOJ/nyoj746.cpp
+++ NYOJ/nyoj746.cpp
@@ -1,23 +1,4 @@
-//Life has never been fucking so easy...
-//...Acm without girlfriend
-#include<iostream>
-#include<vector>
-#include<set>
-#include<string>
-#include<queue>
-#include<stack>
-#include<map>
-#include<algorithm>
-#include<functional>
-#include<memory>
-#include<utility>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<climits>
-#include<cctype>
-#include<cstdlib>
-#include<iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> PII;
