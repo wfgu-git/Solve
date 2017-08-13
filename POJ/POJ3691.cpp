58952b71d45ce627905d0716d67881b421dfb268
Prgwf <peter.wfgu@gmail.com>
2017-08-14 00:53:16 +0800

    add...

--- POJ/POJ3691.cpp
+++ POJ/POJ3691.cpp
@@ -1,11 +1,20 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <algorithm>
 using namespace std;
 const int maxn = 500000 + 20;
-const int chsz = 26;
+const int chsz = 4;
 struct Tire {
   int tot;
   int root;
   int fail[maxn], next[maxn][chsz], end[maxn];
+  int getid(char c) {
+    if (c == 'A') return 0;
+    else if (c == 'T') return 1;
+    else if (c == 'C') return 2;
+    return 3;
+  }
   int newnode() {
     for (int i = 0; i < chsz; ++i) {
       next[tot][i] = -1;
@@ -22,7 +31,7 @@ struct Tire {
   void insert(const char* str) {
     int now = root;
     while (*str) {
-      int chid = *str - 'a';
+      int chid = getid(*str);
       if (next[now][chid] == -1) next[now][chid] = newnode();
       now = next[now][chid];
       ++str;
@@ -47,9 +56,9 @@ struct Tire {
       for (int i = 0; i < chsz; ++i) {
         if (next[now][i] == -1) {
           next[now][i] = next[fail[now]][i];
-          printf("%c is not exist refer to %c\n", i + 'a', i + 'a');
         } else {
           fail[next[now][i]] = next[fail[now]][i];
+          if (end[fail[next[now][i]]]) end[next[now][i]] = 1;
           q.push(next[now][i]);
         }
       }
@@ -73,24 +82,48 @@ struct Tire {
   }
 
 } AC;
+
+const int inf = 0x3f3f3f3f;
 int N;
-inline void work() {
-  static char buf[1000020];
-  scanf("%d", &N);
+int dp[1000 + 20][1000 + 20];
+inline void work(int kase) {
   AC.init();
+  static char buf[1000 + 20];
   for (int i = 0; i < N; ++i) {
     scanf("%s", buf);
     AC.insert(buf);
   }
   AC.build();
   scanf("%s", buf);
-  printf("%d\n", AC.solve(buf));
+  int n = strlen(buf);
+
+  for (int i = 0; i <= n; ++i) {
+    for (int j = 0; j < AC.tot; ++j) {
+      dp[i][j] = inf;
+    }
+  }
+  dp[0][0] = 0;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 0; j < AC.tot; ++j) {
+      if (AC.end[j] || dp[i - 1][j] == inf) continue;
+      for (int k = 0; k < 4; ++k) {
+        int t = AC.next[j][k];
+        if (AC.end[t]) continue;
+        dp[i][t] = min(dp[i][t], dp[i - 1][j] + !(AC.getid(buf[i - 1]) == k));
+      }
+    }
+  }
+  int ans = inf;
+  for (int i = 0; i < AC.tot; ++i) {
+    ans = min(ans, dp[n][i]);
+  }
+  ans = (ans == inf ? -1 : ans);
+  printf("Case %d: %d\n", kase, ans);
 }
 int main() {
-  int T;
-  scanf("%d", &T);
-  for (int cas = 1; cas <= T; ++cas) {
-    work();
+  int kase = 0;
+  while (scanf("%d", &N) != EOF && N) {
+    work(++kase);
   }
   return 0;
 }
