bc2924ae22a66eb8fb48b79370aa14da517d1fe4
Prgwf <peter.wfgu@gmail.com>
2017-09-29 23:21:22 +0800

    add...

--- Codeforces/156/C.cpp
+++ Codeforces/156/C.cpp
@@ -6,18 +6,37 @@ using namespace std;
 
 typedef long long ll;
 typedef long double ld;
+inline int read() {
+  char c = getchar();
+  int x = 0, f = 1;
+  while (c < '0' || c > '9') {
+    if (c == '-') f = -1;
+    c = getchar();
+  }
+  while (c >= '0' && c <= '9') {
+    x = x * 10 + c - '0';
+    c = getchar();
+  }
+  return x * f;
+}
 
 const int inf = 0x3f3f3f3f;
-const int maxn = 100000 + 20;
+const int maxn = 4000 + 20;
 
-int b[4001];
-int dp[4001];
+int n;
+int a[maxn], b[maxn];
+int dp[maxn][maxn];
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int n;
-  cin >> n;
-  for (int i = 1; i <= n; ++i) {
-    cin >> b[i];
+  n = read();
+  for (int i = 0; i < n; ++i) {
+    a[i] = read();
+    b[i] = a[i];
+  }
+  sort(b, b + n);
+  int tot = unique(b, b + n) - b;
+  for (int i = 0; i < n; ++i) {
+    a[i] = lower_bound(b, b + tot, a[i]) - b;
+  }
+  for (int i = 0; i < n; ++i) {
   }
 }
