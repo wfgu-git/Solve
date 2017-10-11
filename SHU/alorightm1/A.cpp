657fa62d24a98acb5b76094424f83250068af1ce
Prgwf <peter.wfgu@gmail.com>
2017-10-11 23:52:10 +0800

    add...

---
+++ SHU/alorightm1/A.cpp
@@ -0,0 +1,66 @@
+/*
+教练我想打ACM！
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const int inf = 0x3f3f3f3f;
+const int maxn = 100 + 20;
+
+int n;
+int p[maxn];
+int dp[maxn][maxn];
+int s[maxn][maxn];
+string ans;
+void dfs(int l, int r) {
+  if (l == r) {
+    ans.push_back('A');
+    ans.push_back(char('0' + l));
+  } else {
+    ans.push_back('(');
+    dfs(l, s[l][r]);
+    dfs(s[l][r] + 1, r);
+    ans.push_back(')');
+  }
+}
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("/home/wfgu/Documents/solve/data.in", "r", stdin);
+  freopen("/home/wfgu/Documents/solve/data.out", "w", stdout);
+#endif
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int kase = 0;
+  while (cin >> n) {
+    for (int i = 0; i <= n; ++i) {
+      cin >> p[i];
+    }
+    memset(dp, 0, sizeof(dp));
+    memset(s, 0, sizeof(s));
+    for (int r = 2; r <= n; ++r) {
+      for (int i = 1; i + r - 1 <= n; ++i) {
+        int j = i + r - 1;
+        dp[i][j] = dp[i + 1][j] + p[i - 1] * p[i] * p[j];
+        s[i][j] = i;
+        for (int k = i + 1; k < j; ++k) {
+          int t = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j];
+          if (t < dp[i][j]) {
+            dp[i][j] = t;
+            s[i][j] = k;
+          }
+        }
+      }
+    }
+    cout << "Case " << ++kase << '\n' << dp[1][n] << " ";
+    ans.clear();
+    dfs(1, n);
+    if (ans[0] == '(') {
+      ans = ans.substr(1, ans.size() - 2);
+    }
+    cout << ans << endl;
+  }
+}
--- SHU/alorightm1/D.cpp
+++ SHU/alorightm1/D.cpp
@@ -11,21 +11,84 @@ typedef pair<int, int> pii;
 const int inf = 0x3f3f3f3f;
 const int maxn = 50 + 20;
 
-int a[maxn];
-int vis[maxn];
+int w[maxn];
+struct node {
+  int w;
+  int fa, lson, rson;
+} ;
+node nds[maxn * 2 + 1];
+string ans[maxn];
+struct comp {
+  bool operator () (const pair<int, int>& x, const pair<int, int>& y) const {
+    if (x.first == y.first) {
+      return x.second < y.second;
+    }
+    return x.first > y.first;
+  }
+} ;
 int main() {
+#ifndef ONLINE_JUDGE
+  freopen("/home/wfgu/Documents/solve/data.in", "r", stdin);
+  freopen("/home/wfgu/Documents/solve/data.out", "w", stdout);
+#endif
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
   int T;
-  scanf("%d", &T);
+  cin >> T;
   for (int cas = 1; cas <= T; ++cas) {
-    memset(vis, 0, sizeof(vis));
     int n;
-    scanf("%d", &n);
-    priority_queue<int, vector<int>, greater<int> > Q;
-    for (int x, i = 0; i < n; ++i) {
-      scanf("%d", &x);
-      Q.push(x);
-      a[i] = x;
+    cin >> n;
+    priority_queue<pii, vector<pii>, comp> Q;
+    for (int i = 0; i < n; ++i) {
+      ans[i].clear();
+    }
+    for (int i = 0; i < n; ++i) {
+      cin >> w[i];
+      nds[i] = {w[i], -1, -1, -1};
+      Q.push({w[i], i});
     }
 
+    for (int i = n; i < 2 * n - 1; ++i) {
+      nds[i] = {0, -1, -1, -1};
+    }
+    pii tr, tl;
+    int trv, tlv;
+    int trid, tlid;
+    for (int i = n; i < 2 * n - 1; ++i) {
+      tr = Q.top(); Q.pop();
+      trv = tr.first;
+      trid = tr.second;
+      tl = Q.top(); Q.pop();
+      tlv = tl.first;
+      tlid = tl.second;
+      nds[tlid].fa = i;
+      nds[trid].fa = i;
+      nds[i].lson = tlid;
+      nds[i].rson = trid;
+      nds[i].w = tlv + trv;
+      Q.push({nds[i].w, i});
+    }
+
+    for (int i = 0; i < n; ++i) {
+      int now = i;
+      int fa = nds[i].fa;
+      while (fa != -1) {
+        if (nds[fa].lson == now) {
+          ans[i].push_back('0');
+        } else {
+          ans[i].push_back('1');
+        }
+        now = fa;
+        fa = nds[now].fa;
+      }
+      reverse(ans[i].begin(), ans[i].end());
+    }
+
+    cout << "Case " << cas << '\n';
+    for (int i = 0; i < n; ++i) {
+      cout << w[i] << " " << ans[i] << '\n';
+    }
+    cout << endl;
   }
 }
