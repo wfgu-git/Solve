41809ee1e5fc7462c66419d072062738ffd73017
Prgwf <peter.wfgu@gmail.com>
2017-10-12 21:39:49 +0800

    add...

---
+++ Codeforces/Gym/101137/L.cpp
@@ -0,0 +1,26 @@
+/*
+教练我想打ACM！
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const int inf = 0x3f3f3f3f;
+const int maxn = 100000 + 20;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("/home/wfgu/Documents/solve/data.in", "r", stdin);
+  freopen("/home/wfgu/Documents/solve/data.out", "w", stdout);
+#endif
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    char opt[2];
+    int day;
+    for (int i = 0; i < n; ++i) {
+      scanf("%s%d", opt, &day);
+    }
+  }
+}
