0fb65e1839f7b28de43fc5bab9eff35f926e89e6
Prgwf <peter.wfgu@gmail.com>
2017-11-09 18:07:33 +0800

    add...

--- Newcoder/WannaFly_20171020/E.cpp
+++ Newcoder/WannaFly_20171020/E.cpp
@@ -5,43 +5,42 @@
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 
-const int inf = 0x3f3f3f3f;
-const int maxn = 100000 + 20;
-
+ll n;
+ll po5[26];
+ll num[26][2];
 void work() {
-    printf("0: 0\n");
-    printf("1: 00000\n");
-    printf("2: 0000011111000001111100000\n");
-    printf("3: 0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("4: 0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    printf("   0000011111000001111100000\n");
-    for(int i=125;i<625;i++)
-    {
-        if(i%25==0) printf("   ");
-        int temp=0;
-        temp+=i/5;
-        temp+=i/25;
-        temp+=i/125;
-        if(temp&1) printf("1");
-        else printf("0");
-        if(i%25==24) printf("\n");
+  po5[0] = 1;
+  for (int i = 1; i <= 25; ++i) {
+    po5[i] = po5[i - 1] * 5;
+  }
+  num[0][0] = 1;
+  num[0][1] = 0;
+  for (int i = 1; i <= 25; ++i) {
+    if (i & 1) {
+      num[i][0] = num[i - 1][0] * 5;
+      num[i][1] = num[i - 1][1] * 5;
+    } else {
+      num[i][0] = 3 * num[i - 1][0] + 2 * num[i - 1][1];
+      num[i][1] = 3 * num[i - 1][1] + 2 * num[i - 1][0];
     }
+  }
+
+  while (cin >> n) {
+    if (n < 0) return;
+    ll ans = 0, tmp = 0;
+    for (int i = 25; i >= 0; --i) {
+      for (int j = 1; j <= n / po5[i]; ++j) {
+        ans += num[i][tmp];
+        if (i & 1) tmp ^= 1;
+      }
+      n %= po5[i];
+    }
+    cout << ans + num[0][tmp] << endl;
+  }
 }
 int main() {
   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-  #ifndef ONLINE_JUDGE
-    freopen("data.in", "r", stdin);
-  #endif
 
   work();
 }
--- SHU/alorightm1/G.cpp
+++ SHU/alorightm1/G.cpp
@@ -4,12 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
 typedef pair<int, int> pii;
-
-const int inf = 0x3f3f3f3f;
-const int maxn = 100000 + 20;
 
 int dist[10][10];
 const int dir[8][2] = {
